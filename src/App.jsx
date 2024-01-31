d483dca393ebd64d95584e9622e74ede053f2830
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-01-30 22:59:04 -0600

    fixes axios

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const { axios } = useAxios()
 
   axios.get('/welcome').then((response) => {
-    console.log(response)
+    console.log(response.data)
   }).catch((error) => {
     console.log(error)
   })
--- src/composables/useAxios.js
+++ src/composables/useAxios.js
@@ -2,21 +2,10 @@ import Axios from 'axios'
 
 export default function () {
     const baseURL = "https://backend-test-harold-4dde42d7a5f3.herokuapp.com"
+    // const baseURL = "http://localhost:8080"
     // const baseURL = import.meta.env.API_URL
 
     const axios = Axios.create({ baseURL, timeout: 30000 })
-
-    axios.interceptors.request.use(
-        config => {
-            config.headers['Content-Type'] = 'application/json'
-            config.headers['Access-Control-Allow-Origin'] = '*'
-            config.headers['Access-Control-Allow-Credentials'] = false
-            return config
-        },
-        error => {
-            return Promise.reject(error)
-        }
-    )
 
     return { axios }
 }
